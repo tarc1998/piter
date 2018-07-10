6bd4a6f4092a3b2063c155f61b84a94bb3b7c38f
Tomek <t.arczewski1998@gmail.com>
2018-07-10 19:08:37 +0200

    final version

--- piter.py
+++ piter.py
@@ -2,11 +2,12 @@ import math
 import numpy as np
 
 n = 5
-m = 5
-iter = 100
+m = 10
+iter = 5
 goal = (0, 0)
 gamma = 1
 
+probab = [0.6, 0.1, 0.1, 0.1]
 dir_r = [-1, 0, 1, 0]
 dir_c = [0, 1, 0, -1]
 v1 = np.array([[0]*m]*n)
@@ -15,48 +16,87 @@ v3 = np.random.rand(n, m)
 
 # reward=np.array([[[-1]*4]*m]*n)
 reward = -np.ones((n,m,4))
-prob = np.array([[[[[0]*m]*n]*m]*n]*4)
-pi = np.array([[[0.25]*m]*n]*4)
+prob = np.array([[[[[0.]*m]*n]*m]*n]*4)
+pi = np.array([[[0.25]*4]*m]*n)
+pi_prime = np.array([[[0.25]*4]*m]*n)
 
 # setting dependent probabilities
 for i in range(0, n):
     for j in range(0, m):
         for k in range(0, 4):
-            prob[k][i][j][i+dir_r[k]][j+dir_c[k]] = 0.6
-            prob[k][i][j][i][j] = 0.1
-            prob[k][i][j][i + dir_r[(k + 1) % 4]][j + dir_c[(k + 1) % 4]] = 0.1
-            prob[k][i][j][i + dir_r[(k + 2) % 4]][j + dir_c[(k + 2) % 4]] = 0.1
-            prob[k][i][j][i + dir_r[(k + 3) % 4]][j + dir_c[(k + 3) % 4]] = 0.1
-
-
+            stay = 1 - sum(probab)
+            for l in range(0, 4):
+                if 0 <= i+dir_r[(k+l) % 4]<n and 0 <= j+dir_c[(k+l) % 4]<m:
+                    prob[k][i][j][i+dir_r[(k+l) % 4]][j+dir_c[(k+l) % 4]]=probab[l]
+                else:
+                    stay += probab[l]
+            prob[k][i][j][i][j] = stay
 
 # the upper left corner keeps
-for dir in range(0, 4):
-    reward[0][0][dir] = 0
+for a in range(0, 4):
+    reward[0][0][a] = 0
     for i in range(0, n):
         for j in range(0, m):
-            prob[dir][0][0][i][j] = 0
-    prob[dir][0][0][0][0] = 1
+            prob[a][0][0][i][j] = 0
+    prob[a][0][0][0][0] = 1
 
-u=1
-for u in range(0,iter):
-    v3=v1
-    u=u+1
-    if(u%1000 == 0): print(u)
-    for i in range(0,n):
-        for j in range(0,m):
-            maks=-1000000
-            for a in range(0,4):
-                # v=reward[i][j][a]+gamma*sum(prob[a][i][j][r][c]*v1[r][c] for c in range(0,m) for r in range(0,n))
-                suma=0
-                for r in range(0,n):
-                    for c in range(0,m):
-                        suma+=prob[a,i,j,r,c]*v1[r,c]
-                v=reward[i][j][a]+gamma*suma
-                if v>maks:
-                    maks=v
-            v2[i][j]=maks
-    v1=v2
-print(u)
-print(v1)
-print(reward)
+# print(prob)
+
+
+def policy_evaluation(pi, iter):
+    v = np.array([[0] * m] * n)
+    v_prime = np.array([[0] * m] * n)
+
+    for it in range(iter):
+        for i in range(n):
+            for j in range(m):
+                suma1 = 0
+                for a in range(4):
+                    suma2 = 0
+                    for r in range(n):
+                        for c in range(m):
+                            suma2 += prob[a][i][j][r][c]*v[r][c]
+                    suma1 += pi[i][j][a]*(reward[i][j][a]+gamma*suma2)
+                v_prime[i][j]=suma1
+        v = v_prime.copy()
+    return v
+
+
+def action_value(v):
+    q = np.ones((n,m,4))
+    for i in range(n):
+        for j in range(m):
+            for a in range(4):
+                suma = 0
+                for r in range(n):
+                    for c in range(m):
+                        suma += prob[a][i][j][r][c]*v[r][c]
+                q[i][j][a]=reward[i][j][a]+gamma*suma
+    return q
+
+
+#print(action_value(policy_evaluation(pi, 100)))
+
+
+def pimproved(q):
+    pi = np.array([[[0.25] * 4] * m] * n)
+    for i in range(n):
+       for j in range(m):
+           ar = np.argmax(q[i][j])
+           for a in range(4):
+               if a == ar:
+                   pi[i][j][a]=1
+               else:
+                   pi[i][j][a]=0
+    return pi
+
+for i in range(iter):
+    pi_prime = pimproved(action_value(policy_evaluation(pi, 100)))
+    pi = pi_prime.copy()
+
+disp = ["U", "R", "D", "L"]
+
+for i in range(n):
+    for j in range(m):
+        print disp[np.argmax(pi[i, j])],
+    print("")
